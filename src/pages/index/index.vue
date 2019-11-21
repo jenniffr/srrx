9e142575ee10db4d4f22974023aabd51d93f9878
jenniffr <1321638673@qq.com>
2019-11-21 20:25:32 +0800

    app

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <i-grid i-class="no-border"> 
-    <i-grid-item v-for="grid in grid" :key="grid" i-class="no-border">
+    <i-grid-item v-for="grid in grids" :key="grid" i-class="no-border">
         <i-grid-icon>
-            <image src="grid.images" />
+            <image :src="grid.image" />
         </i-grid-icon>
         <i-grid-label>{{grid.title}}</i-grid-label>
     </i-grid-item>
@@ -11,15 +11,15 @@
     </i-grid>
     <i-panel :title="title_name">
     <view style="padding: 15px;">
-      <i-card title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+      <i-card i-class="split" title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
          <view slot="content">内容不错</view>
          <view slot="footer">尾部内容</view>
       </i-card>
-      <i-card title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+      <i-card i-class="split" title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
          <view slot="content">内容不错</view>
          <view slot="footer">尾部内容</view>
       </i-card>
-      <i-card title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
+      <i-card i-class="split" title="卡片标题" extra="额外内容" thumb="https://i.loli.net/2017/08/21/599a521472424.jpg">
          <view slot="content">内容不错</view>
          <view slot="footer">尾部内容</view>
       </i-card>
@@ -35,14 +35,14 @@ export default {
     return {
       title_name:"热门",
       grids:[
-        {title:"人物",images:"/static/images/1.png"},
-        {title:"歌曲",images:"/static/images/2.png"},
-        {title:"视频",images:"/static/images/3.png"},
+        {title:"人物",image:"/static/images/1.png"},
+        {title:"歌曲",image:"/static/images/2.png"},
+        {title:"视频",image:"/static/images/3.png"}
       ],
       top:[
-        {name:"歌曲名",singer:"姓名1",image:"图片1"},
-        {name:"歌曲名2",singer:"姓名2",image:"图片2"},
-        {name:"歌曲名3",singer:"姓名3",image:"图片3"},
+        {name:"歌曲名1",singer:"姓名1",image:"图片1",remark:"介绍1"},
+        {name:"歌曲名2",singer:"姓名2",image:"图片2",remark:"介绍2"},
+        {name:"歌曲名3",singer:"姓名3",image:"图片3",remark:"介绍3"},
       ]
     }
   },
